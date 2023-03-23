c786fc51df6f32ce79a57d01ffa3bbac70c4cd60
maxim.stepanov <maxim.stepanov@4finance.com>
2023-03-23 10:43:08 +0200

    create egg component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CountdownComponent } from './countdown/countdown.component';
+import { EggComponent } from './countdown/egg/egg.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CountdownComponent
+    CountdownComponent,
+    EggComponent
   ],
   imports: [
     BrowserModule,
